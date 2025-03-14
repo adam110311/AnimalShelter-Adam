3c214a7c23c4133bcb8c9cdf1a0f420e17b3d321
adam110311 <70455243+adam110311@users.noreply.github.com>
2025-03-14 10:16:47 +0100

    Add project files.

---
+++ AnimalShelter/AdministrationForm.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace AnimalShelter
+{
+    partial class AdministrationForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.animalTypeComboBox = new System.Windows.Forms.ComboBox();
+            this.createAnimalButton = new System.Windows.Forms.Button();
+            this.showInfoButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // animalTypeComboBox
+            // 
+            this.animalTypeComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.animalTypeComboBox.FormattingEnabled = true;
+            this.animalTypeComboBox.Items.AddRange(new object[] {
+            "Cat",
+            "Dog"});
+            this.animalTypeComboBox.Location = new System.Drawing.Point(12, 12);
+            this.animalTypeComboBox.Name = "animalTypeComboBox";
+            this.animalTypeComboBox.Size = new System.Drawing.Size(121, 21);
+            this.animalTypeComboBox.TabIndex = 0;
+            // 
+            // createAnimalButton
+            // 
+            this.createAnimalButton.Location = new System.Drawing.Point(139, 10);
+            this.createAnimalButton.Name = "createAnimalButton";
+            this.createAnimalButton.Size = new System.Drawing.Size(64, 23);
+            this.createAnimalButton.TabIndex = 1;
+            this.createAnimalButton.Text = "Create";
+            this.createAnimalButton.UseVisualStyleBackColor = true;
+            this.createAnimalButton.Click += new System.EventHandler(this.createAnimalButton_Click);
+            // 
+            // showInfoButton
+            // 
+            this.showInfoButton.Location = new System.Drawing.Point(209, 10);
+            this.showInfoButton.Name = "showInfoButton";
+            this.showInfoButton.Size = new System.Drawing.Size(75, 23);
+            this.showInfoButton.TabIndex = 2;
+            this.showInfoButton.Text = "Show info";
+            this.showInfoButton.UseVisualStyleBackColor = true;
+            this.showInfoButton.Click += new System.EventHandler(this.showInfoButton_Click);
+            // 
+            // AdministrationForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(598, 327);
+            this.Controls.Add(this.showInfoButton);
+            this.Controls.Add(this.createAnimalButton);
+            this.Controls.Add(this.animalTypeComboBox);
+            this.Name = "AdministrationForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox animalTypeComboBox;
+        private System.Windows.Forms.Button createAnimalButton;
+        private System.Windows.Forms.Button showInfoButton;
+
+    }
+}
+
